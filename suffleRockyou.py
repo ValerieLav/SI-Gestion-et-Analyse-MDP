f1f738980f60807dffa63f993e564ddcbc865c64
ValerieLav <kajanglavalerie@gmail.com>
2024-11-04 12:20:30 +0100

    Mélange du fichier RockYou.txt

---
+++ suffleRockyou.py
@@ -0,0 +1,18 @@
+##########################################
+#                                        #
+#     Mélange du Fichier RockYou.txt     #
+#                                        #
+##########################################
+import pandas as pd
+
+pathfile = r'rockyou.txt'
+df_no_suffle = pd.read_csv(pathfile,
+                           on_bad_lines='skip',
+                           encoding="latin-1",
+                           header=None,
+                           names=["pwd"])
+suffled_df = df_no_suffle.sample(frac=1).reset_index(drop=True)
+suffled_df.to_csv(f'rockyou.txt')
+
+# Test
+print(suffled_df.iloc[0])
